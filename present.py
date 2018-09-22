e63c975d100e4e137884b28a754a12aa4672d66c
thomas83470 <estival.t@hotmail.com>
2018-09-22 22:46:18 +0200

    affichage optimal des produits

---
+++ present.py
@@ -0,0 +1,6 @@
+# coding: utf-8
+texte = "[{'NOM': 'Bordeaux superieur', 'PRODUIT_ID': 276}, {'NOM': 'Château Lataste', 'PRODUIT_ID': 279}, {'NOM': '', 'PRODUIT_ID': 281}, {'NOM': 'Domaine du Bourdier Bordeaux', 'PRODUIT_ID': 284}, {'NOM': 'Chateau La Croix Bigorre', 'PRODUIT_ID': 286}, {'NOM': 'Bordeaux élevé en fûts de chêne', 'PRODUIT_ID': 289}, {'NOM': 'Bordeaux Clairet', 'PRODUIT_ID': 291}, {'NOM': 'Bordeaux 2016', 'PRODUIT_ID': 294}, {'NOM': 'CHATEAU CARBONNIEUX GRAND CRU CLASSE DE GRAVES PESSAC - LEOGNAN', 'PRODUIT_ID': 296}, {'NOM': 'chateau ferrande ', 'PRODUIT_ID': 299}]"
+texte = texte.lower()
+texte = texte.replace('{','\n')
+texte = texte.replace('}','')
+print(texte)
